f7b21ca14d119d3d6e2c5607923de532e4eea084
Tobias Stöckler <tstoeckler@gmail.com>
2016-04-06 01:49:38 +0200

    Update the entity-field-updates route

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,10 +2,51 @@
 
 namespace Drupal\event\Controller;
 
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Drupal\Core\Entity\EntityDefinitionUpdateManagerInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
 /**
  * Provides a test controller.
  */
-class TestController {
+class TestController implements ContainerInjectionInterface {
+
+  /**
+   * The entity definition update manager.
+   *
+   * @var \Drupal\Core\Entity\EntityDefinitionUpdateManagerInterface
+   */
+  protected $entityDefinitionUpdateManager;
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Constructs a TestController object.
+   *
+   * @param \Drupal\Core\Entity\EntityDefinitionUpdateManagerInterface $entity_definition_update_manager
+   *   The entity definition update manager.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   */
+  public function __construct(EntityDefinitionUpdateManagerInterface $entity_definition_update_manager, EntityTypeManagerInterface $entity_type_manager) {
+    $this->entityDefinitionUpdateManager = $entity_definition_update_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity.definition_update_manager'),
+      $container->get('entity_type.manager')
+    );
+  }
 
   /**
    * Provides an empty test controller to easily execute arbitrary code.
@@ -21,7 +62,37 @@ class TestController {
 
     // Place any test code here.
 
-    return ['content' => ['#markup' => 'Any code placed in \\' . __METHOD__ . '() is executed on this page.']];
+    return ['#markup' => 'Any code placed in \\' . __METHOD__ . '() is executed on this page.'];
+  }
+
+  /**
+   * Provides a test controller to updates entity/field definitions.
+   *
+   * @return array
+   *   A renderable array that contains a summary of the applied entity/field
+   *   definitions.
+   */
+  public function updateEntityFieldDefinitions() {
+    $build = [];
+
+    if ($change_summary = $this->entityDefinitionUpdateManager->getChangeSummary()) {
+      $build[] = ['#markup' => 'Outstanding entity/field changes:'];
+      foreach ($change_summary as $entity_type_id => $changes) {
+        $build[] = [
+          '#theme' => 'item_list',
+          '#title' => $this->entityTypeManager->getDefinition($entity_type_id)->getLabel(),
+          '#items' => $changes,
+        ];
+      }
+      $this->entityDefinitionUpdateManager->applyUpdates();
+
+      drupal_set_message('Entity/field updates depicted below successfully applied.');
+    }
+    else {
+      $build[] = ['#markup' => 'No outstanding entity/field definition updates.'];
+    }
+
+    return $build;
   }
 
 }
