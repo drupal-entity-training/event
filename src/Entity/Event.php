decb80e9d7a71d435570cf1f2aa929c889518d2d
Tobias Stöckler <tstoeckler@gmail.com>
2016-04-06 01:15:14 +0200

    Add some basic base field definitions

--- src/Entity/Event.php
+++ src/Entity/Event.php
@@ -3,6 +3,8 @@
 namespace Drupal\event\Entity;
 
 use Drupal\Core\Entity\ContentEntityBase;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Field\BaseFieldDefinition;
 
 /**
  * Defines the event entity.
@@ -17,5 +19,20 @@ use Drupal\Core\Entity\ContentEntityBase;
  * )
  */
 class Event extends ContentEntityBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
+    $fields = parent::baseFieldDefinitions($entity_type);
+
+    $fields['title'] = BaseFieldDefinition::create('string');
+
+    $fields['date'] = BaseFieldDefinition::create('datetime');
+
+    $fields['description'] = BaseFieldDefinition::create('text_long');
+
+    return $fields;
+  }
 
 }
