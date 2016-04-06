3b9bac700270ef5ab683ed8803f0dfe5dbae98d0
Tobias Stöckler <tstoeckler@gmail.com>
2016-04-06 10:55:31 +0200

    Merge 1

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,34 +2,131 @@
 
 namespace Drupal\event\Controller;
 
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Drupal\Core\Entity\EntityDefinitionUpdateManagerInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\event\Entity\Event;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides a test controller.
  */
-class TestController {
+class TestController implements ContainerInjectionInterface {
+
+  /**
+   * The entity definition update manager.
+   *
+   * @var \Drupal\Core\Entity\EntityDefinitionUpdateManagerInterface
+   */
+  protected $entityDefinitionUpdateManager;
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Constructs a TestController object.
+   *
+   * @param \Drupal\Core\Entity\EntityDefinitionUpdateManagerInterface $entity_definition_update_manager
+   *   The entity definition update manager.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   */
+  public function __construct(EntityDefinitionUpdateManagerInterface $entity_definition_update_manager, EntityTypeManagerInterface $entity_type_manager) {
+    $this->entityDefinitionUpdateManager = $entity_definition_update_manager;
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity.definition_update_manager'),
+      $container->get('entity_type.manager')
+    );
+  }
 
   /**
    * Provides an empty test controller to easily execute arbitrary code.
    *
    * This is exposed at the '/test' path on your site.
    *
+   * If Drush is available you can also run arbitrary code in the context of a
+   * bootstrapped Drupal site with the "drupal php-eval" (or "drush ev")
+   * command.
+   *
    * @return array
    *   A renderable array that contains instruction text for this controller.
    *
    * @see event.routing.yml
    */
-  public function test() {
+  public function evaluateTestCode() {
 
     // This creates a new event and saves it to the database:
-    // Event::create()->save();
+    // $event = Event::create();
+    // $event->save();
+    // drupal_set_message('A new event with the ID ' . $event->id() . ' has been saved.');
 
     // This loads an event by its ID and displays its UUID in a message.
     // $id = 1;
     // $uuid = Event::load($id)->uuid();
-    // drupal_set_message('The UUID for event with ID ' . $id . ' is ' . $uuid . '.');
+    // drupal_set_message('The UUID for event with the ID ' . $id . ' is ' . $uuid . '.');
 
-    return ['content' => ['#markup' => 'Any code placed in \\' . __METHOD__ . '() is executed on this page.']];
+    // This deletes an event by its ID.
+    // $id = 1;
+    // Event::load($id)->delete();
+    // drupal_set_message('The event with the ID ' . $id . ' was successfully deleted.');
+
+    return ['#markup' => 'Any code placed in \\' . __METHOD__ . '() is executed on this page.'];
+  }
+
+  /**
+   * Provides a test controller to update entity/field definitions.
+   *
+   * This is exposed at the '/update-entity-field-definitions' path on your
+   * site.
+   *
+   * If Drush is available, this can be achieved by running
+   * "drush entity-updates" (or "drush entup") instead.
+   *
+   * @return array
+   *   A renderable array that contains a summary of the applied entity/field
+   *   definitions.
+   *
+   * @see \Drupal\Core\Entity\EntityDefinitionUpdateManagerInterface::applyUpdates()
+   */
+  public function updateEntityFieldDefinitions() {
+    $build = [];
+
+    // This code mimics the code that displays the list of needed entity/field
+    // definition updates on the status report at /admin/reports/status.
+    /** @see system_requirements() */
+    if ($change_summary = $this->entityDefinitionUpdateManager->getChangeSummary()) {
+      foreach ($change_summary as $entity_type_id => $changes) {
+        $build[] = [
+          '#theme' => 'item_list',
+          '#title' => $this->entityTypeManager->getDefinition($entity_type_id)->getLabel(),
+          '#items' => $changes,
+        ];
+      }
+
+      // This line of code is the only one that is not related to the output of
+      // this controller. It proves that the functionality to update the
+      // entity/field definitions is given by Drupal core itself although no UI
+      // exists for it at this point.
+      $this->entityDefinitionUpdateManager->applyUpdates();
+
+      drupal_set_message('The entity/field definition updates listed below have been applied successfully.');
+    }
+    else {
+      $build[] = ['#markup' => 'No outstanding entity/field definition updates.'];
+    }
+
+    return $build;
   }
 
 }
