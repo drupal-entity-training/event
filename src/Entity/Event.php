680638e0433b5d717340e517704ddde601f6dfde
Tobias Stöckler <tstoeckler@gmail.com>
2016-05-05 11:35:51 +0200

    Change weights to be in 5s

--- src/Entity/Event.php
+++ src/Entity/Event.php
@@ -111,7 +111,7 @@ class Event extends ContentEntityBase implements EventInterface {
       ->setSetting('datetime_type', DateTimeItem::DATETIME_TYPE_DATE)
       ->setRequired(TRUE)
       ->setDisplayOptions('form', [
-        'weight' => 10,
+        'weight' => 5,
       ])
       ->setDisplayOptions('view', [
         'label' => 'inline',
@@ -121,11 +121,11 @@ class Event extends ContentEntityBase implements EventInterface {
     $fields['description'] = BaseFieldDefinition::create('text_long')
       ->setLabel(t('Description'))
       ->setDisplayOptions('form', [
-        'weight' => 20,
+        'weight' => 10,
       ])
       ->setDisplayOptions('view', [
         'label' => 'hidden',
-        'weight' => 10,
+        'weight' => 5,
       ]);
 
     return $fields;
